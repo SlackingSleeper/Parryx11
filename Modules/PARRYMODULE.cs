fce769b718737d101f21c6af889d31c2a796b1b1
Mark Westerlund <mana.torsk@gmail.com>
2026-05-07 15:09:29 +0200

    PARRYx11 INITIAL COMMIT

---
+++ Modules/PARRYMODULE.cs
@@ -0,0 +1,40 @@
+using NeonLite.Modules;
+using UnityEngine;
+
+namespace ParryParryParryParryParryParryParryParryParryParryParry.Modules
+{
+    // This is an example template module that does nothing but print things to log every few seconds.
+    // Check NeonLite/Modules/IModule.cs for more information.
+    // Make sure to remove the wrapped #if false when you copy this!
+    internal class PARRYMODULE : MonoBehaviour, IModule
+    {
+        // A "true" priority means it'll start before the low priority mods (before the main menu loads.)
+        // This uses a holder, so Activate gets called later.
+        const bool priority = false;
+        static bool active = false;
+
+        // This will be called once at the start of the game.
+        // All mods will be setup at the same time, no matter their priority.
+        static void Setup()
+        {
+            // This is how you would create a toggle setting using the Settings framework.
+            // An empty string as the first argument means it goes into the main category.
+            var setting = Settings.Add(global::ParryParryParryParryParryParryParryParryParryParryParry.ParryParryParryParryParryParryParryParryParryParryParry.Settings.wantActivated, "", "PARRRRTY TIME", "ACTIVATE THE PARRYNATOR", "", true);
+            active = setting.SetupForModule(Activate, static (_, after) => after);
+        }
+        // Activate will be called either at the start of the game or on mod menu setup depending on the priority.
+        // It may be called because of a setting, passed with a bool that says whether or not to activate it.
+        // Here is where you should handle (un)patching using Patching.TogglePatch and component addition and destruction.
+        static void Activate(bool activate)
+        {
+            Patching.TogglePatch(activate, typeof(FirstPersonDrifter).GetMethod("AddVelocity"), ParryParryParryParryParryParryParryParryParryParryParry, Patching.PatchTarget.Prefix);
+
+            active = activate;
+        }
+        static bool ParryParryParryParryParryParryParryParryParryParryParry(Vector3 vel,ref Vector3 ___velocity)
+        {
+            ___velocity += vel;
+            return false;
+        }
+    }
+}
---
+++ YOU KNOW WHAT MOD THIS IS.cs
@@ -0,0 +1,32 @@
+﻿using MelonLoader;
+using UnityEngine;
+
+namespace ParryParryParryParryParryParryParryParryParryParryParry
+{
+    public class ParryParryParryParryParryParryParryParryParryParryParry : MelonMod
+    {
+        internal static ParryParryParryParryParryParryParryParryParryParryParry instance;
+        internal static MelonLogger.Instance Log => instance.LoggerInstance;
+
+        public override void OnInitializeMelon()
+        {
+            instance = this;
+
+            // Register the settings
+            Settings.Register();
+
+            NeonLite.Modules.Anticheat.Register(MelonAssembly);
+            // Load all modules tagged with the IModule interface
+            NeonLite.NeonLite.LoadModules(MelonAssembly);
+        }
+        internal static class Settings
+        {
+            public const string wantActivated = "ParryParryParryParryParryParryParryParryParryParryParry";
+
+            public static void Register()
+            {
+                NeonLite.Settings.AddHolder(wantActivated);
+            }
+        }
+    }
+}
